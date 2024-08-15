a734c84c8ba2ab0b6045ebf8fe8f4d9925f013e0
keith <keithneo00@gmail.com>
2024-08-16 01:17:03 +0800

    First push to git repo

---
+++ app/room/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    <div><h1>This is the tarot room</h1></div>
+  )
+}
+
+export default Room
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>hello this is a test</div>
+  )
+}
+
+export default test
