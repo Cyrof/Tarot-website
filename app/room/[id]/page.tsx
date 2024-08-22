7722f3c84eb4c31d83b562ef9a56ee967808f557
keith <keithneo00@gmail.com>
2024-08-22 17:57:27 +0800

    Update: updated getRotationStyle function in room page

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react'
 import { useParams, usePathname } from 'next/navigation';
 import { useState } from 'react';
 import Image from 'next/image';
+import { get } from 'http';
 
 const Room = () => {
   const { id } = useParams();
@@ -29,9 +30,37 @@ const Room = () => {
     setMessage(`${buttonName} button is clicked on room ${id}`);
   }
 
+  const getRotationStyle = (index: number, total: number) => {
+    const maxAngle = 90; // max angle for the fanned effect
+    const angle = maxAngle * (index / (total - 1)) - maxAngle / 2;
+
+    const cardWidth = 160; // width of the cards (in pixels)
+    const spacing = cardWidth / 2; // spacing between cards
+    const totalWidth = spacing * (total - 1); // calculate total width of the fanned cards
+    const xOffset = index * (window.innerWidth / total);
+    // const xOffset = (index * spacing) - (totalWidth / 2);
+    // const xOffset = (index - (total -1) / 2) * (cardWidth / 2); // center each card
+    console.log(`xOffset: ${xOffset}`);
+
+    // arc calculation 
+    const radius = 400;
+    const yOffset = Math.pow((index - (total - 1) / 2) / ((total - 1) / 2), 2) * radius;
+    console.log(`yOffset: ${yOffset}`);
+
+    return {
+      transform: `rotate(${angle}deg) translate(-50%, -50%)`, // rotate the center around around midpoint
+      // left: `calc(50% + ${xOffset}px)`, // center based on xOffset
+      // top: `calc(50% + ${yOffset})px`, // center based on yOffset
+      // left: `${50 + xOffset}px`,
+      // transform: `rotate(${angle}deg)`,
+      left: `${xOffset}px`,
+      top: `${yOffset}px`,
+    }
+  };
+
 
   return (
-    <div>
+    <div className='m-0 h-screen w-screen'>
       {/* <h1>Room ID: {id}</h1> */}
       <div>
         <h1>Room ID: {id}</h1>
@@ -51,18 +80,25 @@ const Room = () => {
       </button>
       {message && <p>{message}</p>}
 
-      <div className='flex '>
-         {images?.map((image, index) => (
-          <Image 
-          key={index} 
-          src={image} 
-          alt={`image_${index}`} 
-          width={500}
-          height={700}
-          className='object-scale-down m-auto h-42 w-40'
-          />
-         ))}
+      {/* <div className='relative h-full w-full bg-blue-300'> */}
+      <div 
+      className='relative h-3/4 w-4/5 bg-blue-300 m-auto flex items-center justify-center'
+      // className='relative h-3/4 w-4/5 bg-blue-300 m-auto'
+      >
+        {images?.map((image, index) => (
+        <Image 
+        key={index} 
+        src={image} 
+        alt={`image_${index}`} 
+        width={120}
+        height={220}
+        className='object-cover absolute'
+        style={getRotationStyle(index, images.length)}
+        />
+        ))}
       </div>
+         
+      {/* </div> */}
     </div>
   )
 }
